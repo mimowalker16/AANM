2c578b9cb952681d9efdabc195529902c55c34f4
Merrakchi Mouayad <mouayadmerrakchi@gmail.com>
2026-08-30 17:09:01 +0100

    feat: remove "À Propos" links from navigation and footer across multiple pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,6 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                about: resolve(__dirname, 'about.html'),
                 activities: resolve(__dirname, 'activities.html'),
                 news: resolve(__dirname, 'news.html'),
                 contact: resolve(__dirname, 'contact.html'),
